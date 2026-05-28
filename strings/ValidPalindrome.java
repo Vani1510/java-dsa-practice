21b2b2a3b735029f8055173f3b4c535a1ed1cc12
Vani Bedia <vanibedia1510@gmail.com>
2026-05-28 15:56:40 +0530

    added code for Valid Palindrome problem

--- strings/ValidPalindrome.java
+++ strings/ValidPalindrome.java
@@ -1,5 +1,19 @@
 package strings;
 
 public class ValidPalindrome {
-    
+    public boolean isPalindrome(String s) {
+        String str=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int left=0;
+        int right=str.length()-1;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+  
 }
+
