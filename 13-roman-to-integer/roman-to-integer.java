d90e9648216ce2d8fdc63a6a3ead7eabf5f856a5
Vani1510 <vanibedia1510@gmail.com>
2026-08-05 14:31:37 +0530

    Time: 4 ms (54.73%) | Memory: 46.4 MB (63.16%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int romanToInt(String s) {
+        // Map to store character to value mappings
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        int result = 0;
+        int length = s.length();
+        
+        for (int i = 0; i < length; i++) {
+            int currentValue = map.get(s.charAt(i));
+            
+            // Check if subtraction rule applies
+            if (i < length - 1 && currentValue < map.get(s.charAt(i + 1))) {
+                result -= currentValue;
+            } else {
+                result += currentValue;
+            }
+        }
+        
+        return result;
+    }
+}
