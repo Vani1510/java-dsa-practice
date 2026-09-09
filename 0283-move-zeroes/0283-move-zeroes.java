4067794549cf3aa0b66caabd7af6a30ddff98d6b
Vani1510 <vanibedia1510@gmail.com>
2026-09-10 01:44:37 +0530

    Time: 2 ms (91.79%), Space: 47.8 MB (57.26%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,14 +3,11 @@ class Solution {
         int insertpos=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
+                int temp=nums[insertpos];
                 nums[insertpos]=nums[i];
+                nums[i]=temp;
                 insertpos++;
             }
         }
-        while(insertpos<nums.length){
-            nums[insertpos]=0;
-            insertpos++;
-             }
-        
     }
 }
