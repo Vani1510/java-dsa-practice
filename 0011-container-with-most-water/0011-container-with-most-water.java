d791d7657a2ce4a806403dc032198b9954294301
Vani1510 <vanibedia1510@gmail.com>
2026-09-10 01:59:43 +0530

    Time: 4 ms (94.9%), Space: 77.4 MB (29.37%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int maxarea=0;
+        int currarea=0;
+        int left=0;
+        int right=height.length -1;
+        while(left<=right){
+            currarea=(Math.min(height[left],height[right]))*(right-left);
+            if(currarea>maxarea){
+                maxarea=currarea;
+            }
+            if(height[left]<height[right]){
+                left++;
+            }
+            else{
+                right--;
+            }
+
+        }
+        return maxarea;
+    }
+}
