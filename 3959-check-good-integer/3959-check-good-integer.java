4a630ca1457276cd528e0a3bdbd92013186035da
Vani1510 <vanibedia1510@gmail.com>
2026-09-26 16:26:40 +0530

    Time: 1 ms (92.62%), Space: 42.4 MB (59.6%) - LeetHub

---
+++ 3959-check-good-integer/3959-check-good-integer.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkGoodInteger(int n) {
+        int rem=0;
+        int digsum=0;
+        int sqsum=0;
+        while(n>0){
+            rem=n%10;
+            digsum+=rem;
+            sqsum+=rem*rem;
+            n=n/10;
+        }
+        if((sqsum-digsum)>=50){
+            return true;
+        }
+        return false;
+    }
+}
